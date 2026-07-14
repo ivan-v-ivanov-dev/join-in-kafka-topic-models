5dd253dc797b237dfea3517294a7763dfb52d3e4
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-07-14 16:08:48 +0300

    Remove setters

--- src/main/java/com/join_in/kafka_models/messages/RegisterNewUser.java
+++ src/main/java/com/join_in/kafka_models/messages/RegisterNewUser.java
@@ -34,19 +34,4 @@ public class RegisterNewUser implements KafkaMessage {
         return password;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }

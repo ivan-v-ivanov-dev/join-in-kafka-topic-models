7625cc50fa75d9247e14bbcbebb32e681fb72167
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-07-14 15:53:01 +0300

    Create RegisterNewUser

--- src/main/java/com/join_in/kafka_models/messages/RegisterNewUser.java
+++ src/main/java/com/join_in/kafka_models/messages/RegisterNewUser.java
@@ -4,4 +4,33 @@ import com.join_in.kafka_models.KafkaMessage;
 
 public class RegisterNewUser implements KafkaMessage {
 
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public RegisterNewUser() {}
+
+    public RegisterNewUser(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }

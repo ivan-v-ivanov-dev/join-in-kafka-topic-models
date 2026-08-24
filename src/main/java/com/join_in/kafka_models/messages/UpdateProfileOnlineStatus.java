5ee03b5828111e82f2df6fd666eb362a758f7289
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-08-24 11:23:18 +0300

    Records implement KafkaMessage

--- src/main/java/com/join_in/kafka_models/messages/UpdateProfileOnlineStatus.java
+++ src/main/java/com/join_in/kafka_models/messages/UpdateProfileOnlineStatus.java
@@ -1,6 +1,8 @@
 package com.join_in.kafka_models.messages;
 
+import com.join_in.kafka_models.KafkaMessage;
+
 public record UpdateProfileOnlineStatus(
         String identity
-) {
+) implements KafkaMessage {
 }

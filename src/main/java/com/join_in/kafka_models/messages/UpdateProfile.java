13be36ed6b62219b8050060d49ae450d52f4ec5e
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-08-26 11:19:04 +0300

    Update UpdateProfile

--- src/main/java/com/join_in/kafka_models/messages/UpdateProfile.java
+++ src/main/java/com/join_in/kafka_models/messages/UpdateProfile.java
@@ -2,7 +2,8 @@ package com.join_in.kafka_models.messages;
 
 import com.join_in.kafka_models.KafkaMessage;
 
-public record UpdateProfile(String firstName, String lastName, String aboutMe, String mobile, String address,
+public record UpdateProfile(String identity, String firstName, String lastName, String aboutMe, String mobile,
+                            String address,
                             String birthDate, String birthYear, String birthplace, String livesIn, String gender,
                             String interestedIn, String language, String joined, String status, String phoneNumber,
                             String website, String socialLink, String hobbies, String work, String professionalSkills,

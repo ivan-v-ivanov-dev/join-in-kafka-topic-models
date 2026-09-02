3b5180b7a26e6b4a23662bfa2624096d2a52d65f
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-09-02 12:45:19 +0300

    Add fields in PostImage

--- src/main/java/com/join_in/kafka_models/messages/PostImage.java
+++ src/main/java/com/join_in/kafka_models/messages/PostImage.java
@@ -3,7 +3,8 @@ package com.join_in.kafka_models.messages;
 import com.join_in.kafka_models.KafkaMessage;
 
 public record PostImage(
-        String identity,
+        String postIdentity,
+        String imageIdentity,
         byte[] imageBytes
 ) implements KafkaMessage {
 }
